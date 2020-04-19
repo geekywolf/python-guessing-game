8fc1a8d3d2c6750fcadfdc1a0a7595f584c3e49d
geekywolf <t4riumph@gmail.com>
2020-04-19 10:51:25 +0100

    refactored code base

--- guessingpame.py
+++ guessingpame.py
@@ -4,15 +4,15 @@ def random_generator(x,y):
     number = random.randint(x,y)
     return number
 
-def easy():
+def game_logic(x, y):
     check = True
-    count = 6
+    count = x
 
-    randomnum = random_generator(0,10)
+    randomnum = random_generator(1,y)
 
     while check:
         try:
-            user = int(input(f"\nYou have {count} tries left\nEnter a number between 1 - 10:    "))
+            user = int(input(f"\nYou have {count} tries left\nEnter a number between 1 - {y - 1}:    "))
         except ValueError:
             print("Please enter a number")
             #check = False
@@ -30,57 +30,6 @@ def easy():
                 print("\nYou have used up all your tries. GAME OVER")
                 check = False
 
-def medium():
-    check = True
-    count = 4
-
-    randomnum = random_generator(0,20)
-
-    while check:
-        try:
-            user = int(input(f"\nYou have {count} tries left\nEnter a number between 1 - 20:    "))
-        except ValueError:
-            print("Please enter a number")
-            #check = False
-        else:
-            if count > 1:
-                if (user != randomnum):
-                    check = False
-                    print("\nThats wrong, try again")
-                    count -= 1
-                    check = True
-                else:
-                    print("\nYou got it right")
-                    check = False
-            else:
-                print("\nYou have used up all your tries. GAME OVER")
-                check = False
-
-def hard():
-    check = True
-    count = 3
-
-    randomnum = random_generator(0,50)
-
-    while check:
-        try:
-            user = int(input(f"\nYou have {count} tries left\nEnter a number between 1 - 50:    "))
-        except ValueError:
-            print("Please enter a number")
-            #check = False
-        else:
-            if count > 1:
-                if (user != randomnum):
-                    check = False
-                    print("\nThats wrong, try again")
-                    count -= 1
-                    check = True
-                else:
-                    print("\nYou got it right")
-                    check = False
-            else:
-                print("\nYou have used up all your tries. GAME OVER")
-                check = False
 
 try:
     initial_page = int(input("Choose a setting.\nEnter 1 for Easy\nEnter 2 for Medium\nEnter 3 for Hard\nChoice :   "))
@@ -89,11 +38,11 @@ except ValueError:
 else:
     if initial_page in range(0,4):
         if initial_page == 1:
-            easy()
+            game_logic(x= 6, y = 11)
         elif initial_page == 2:
-            medium()
+            game_logic(x = 4, y = 21)
         else:
-            hard()
+            game_logic(x= 3, y = 51)
     else:
         print("Not an option")
         
